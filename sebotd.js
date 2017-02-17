325d3fa3917f58190972a1107fb3a38059ab4594
Joe Hitchens <joe@sleepless.com>
2017-02-17 14:10:01 -0800

    -

--- sebotd.js
+++ sebotd.js
@@ -1,5 +1,23 @@
 
 
+// Standard node.js modules
+var exec = require("child_process").exec;
+var util = require("util");
+var fs = require("fs");
+var path = require("path");
+var http = require('http')
+var url = require("url")
+
+// 3rd partynode.js modules
+var send = require("send");
+
+// Sleepless Inc. modules
 require("sleepless")
 require("g")("log5");
+
+var app = http.createServer(function onRequest (req, res) {
+	var u = url.parse(req);
+	I("u="+o2j(u));
+	send(req, u.pathname).pipe(res)
+}).listen(12345)
 
